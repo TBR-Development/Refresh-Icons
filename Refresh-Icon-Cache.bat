6a342efc8d27b6510306ad07b07aef7d23b1070a
Nimbi <JoshuaVL1988@protonmail.com>
2022-09-25 15:19:15 -0700

    :star: 09/25/2022 - 03:19 PM

---
+++ Refresh-Icon-Cache.bat
@@ -0,0 +1,10 @@
+TASKKILL /f /im explorer.exe
+CD /d %userprofile%\AppData\Local
+DEL IconCache.db /a
+CD /d %userprofile%\AppData\Local\Microsoft\Windows
+del /f /s /q Explorer\*.*
+del /f /s /q Explorer\iconcache*.*
+del /f /s /q Explorer\thumbcache*.*
+DEL /f /s /q Explorer\thumbcache*.* /a
+START explorer.exe
+EXIT
